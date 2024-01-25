f23552a7c9f9587fc360c9adf5ed2069fc3884c7
ryuhah <137030818+ryuhah@users.noreply.github.com>
2024-01-25 15:22:02 +0900

    테스트

---
+++ app/src/main/java/com/example/help_housework/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.help_housework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // 류하경 테스트
+    }
+}
