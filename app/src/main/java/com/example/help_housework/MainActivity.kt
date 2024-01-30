af7c484b3901a78a49666bf400eb5588f6435289
kwy6293 <kwy6293@naver.com>
2024-01-30 17:58:43 +0900

    바텀네비게이션뷰 추가

--- app/src/main/java/com/example/help_housework/MainActivity.kt
+++ app/src/main/java/com/example/help_housework/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.help_housework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            // Handle item selection here
+            item.isChecked = true // This line is important to update the checked state
+            true
+        }
     }
 }
