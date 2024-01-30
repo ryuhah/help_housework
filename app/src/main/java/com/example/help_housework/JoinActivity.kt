12cfba673ea274699bd8a690cc21053bca716741
ryuhah <137030818+ryuhah@users.noreply.github.com>
2024-01-30 17:00:20 +0900

    회원가입 & 로그인 기능

--- app/src/main/java/com/example/help_housework/JoinActivity.kt
+++ app/src/main/java/com/example/help_housework/JoinActivity.kt
@@ -1,11 +1,40 @@
 package com.example.help_housework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.help_housework.databinding.ActivityJoinBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
+private lateinit var auth: FirebaseAuth
 class JoinActivity : AppCompatActivity() {
+    private var mBinding : ActivityJoinBinding ?= null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+        mBinding = ActivityJoinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.btnJoinJ.setOnClickListener{
+            val id = binding.etIdJ.text.toString()
+            val pw = binding.etPwJ.text.toString()
+
+            auth.createUserWithEmailAndPassword(id, pw)
+                .addOnCompleteListener{task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this,"회원가입에 성공했습니다!", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "회원가입에 실패했습니다.${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 }
--- app/src/main/java/com/example/help_housework/LoginActivity.kt
+++ app/src/main/java/com/example/help_housework/LoginActivity.kt
@@ -1,11 +1,44 @@
 package com.example.help_housework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.help_housework.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
+private lateinit var auth: FirebaseAuth
 class LoginActivity : AppCompatActivity() {
+    private var mBinding : ActivityLoginBinding ? = null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.btnLogin.setOnClickListener{
+            val id = binding.etIdL.text.toString()
+            val pw = binding.etPwL.text.toString()
+
+            auth.signInWithEmailAndPassword(id, pw)
+                .addOnCompleteListener {task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "로그인에 성공했습니다!", Toast.LENGTH_SHORT).show()
+                        val mainintent = Intent(this, MainActivity::class.java)
+                        startActivity(mainintent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "아이디와 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        binding.tvJoinL.setOnClickListener{
+            val joinintent = Intent(this, JoinActivity::class.java)
+            startActivity(joinintent)
+        }
     }
 }
