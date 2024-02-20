01f78bdc530afe2040b09f32e2f6e80cbc5d0db1
ryuhah <137030818+ryuhah@users.noreply.github.com>
2024-02-20 18:12:15 +0900

    사주세요 글작성 액티비티 생성 및 레이아웃

---
+++ app/src/main/java/com/example/help_housework/AddBuymeActivity.kt
@@ -0,0 +1,46 @@
+package com.example.help_housework
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.help_housework.databinding.ActivityAddBuymeBinding
+
+class AddBuymeActivity : AppCompatActivity() {
+    private var mBinding : ActivityAddBuymeBinding ?= null
+    private val binding get() = mBinding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityAddBuymeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.title = "사주세요 글쓰기"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    // 툴바 메뉴 버튼을 설정
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.add_buyme_menu,menu)
+        return true
+    }
+
+    // 툴바 메뉴 버튼이 클릭 됐을 때 콜백
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
--- app/src/main/java/com/example/help_housework/BuymeFragment.kt
+++ app/src/main/java/com/example/help_housework/BuymeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.help_housework
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -39,6 +40,10 @@ class BuymeFragment : Fragment() {
             }
         }
 
+        binding.btnFloating.setOnClickListener {
+            val intent = Intent(requireContext(),AddBuymeActivity::class.java)
+            startActivity(intent)
+        }
 
         return view
     }
