17b8f6c56ce384890fcb75b6a670cc7f0d6d080c
ryuhah <137030818+ryuhah@users.noreply.github.com>
2024-02-21 17:56:48 +0900

    초대코드 입력 확인 후 액티비티 실행 수정

--- app/src/main/java/com/example/help_housework/InviteCodeActivity.kt
+++ app/src/main/java/com/example/help_housework/InviteCodeActivity.kt
@@ -6,48 +6,107 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.help_housework.databinding.ActivityInviteCodeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class InviteCodeActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+    private lateinit var database : DatabaseReference
+
     private var mBinding : ActivityInviteCodeBinding ?= null
     private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val isFirstRun = getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE)
-            .getBoolean("isFirstRun", true)
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
 
-        if(isFirstRun){
-            mBinding = ActivityInviteCodeBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+        mBinding = ActivityInviteCodeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            setFrag(0)
-            binding.btnEnterCode.setTextColor(Color.parseColor("#FFE600"))
-
-
-            binding.btnEnterCode.setOnClickListener{
-                setFrag(0)
-                binding.btnEnterCode.setTextColor(Color.parseColor("#FFE600"))
-                binding.btnGenerateCode.setTextColor(Color.WHITE)
-            }
-
-            binding.btnGenerateCode.setOnClickListener{
-                setFrag(1)
-                binding.btnEnterCode.setTextColor(Color.WHITE)
-                binding.btnGenerateCode.setTextColor(Color.parseColor("#FFE600"))
-            }
-
-            getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE)
-                .edit()
-                .putBoolean("isFirstRun", false)
-                .apply()
-        } else {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-
+        getInvitationCode()
     }
+
+    private fun getInvitationCode() {
+        val invitationRef = database.child("invitations")
+
+        invitationRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (invitationSnapshot in dataSnapshot.children) {
+                        val invitationCode = invitationSnapshot.key
+                        if (invitationCode != null) {
+                            getUserUIDFromMeetups(invitationCode)
+                        }
+                    }
+                } else {
+                    setFrag(0)
+                    binding.btnEnterCode.setTextColor(Color.parseColor("#FFE600"))
+
+
+                    binding.btnEnterCode.setOnClickListener{
+                        setFrag(0)
+                        binding.btnEnterCode.setTextColor(Color.parseColor("#FFE600"))
+                        binding.btnGenerateCode.setTextColor(Color.WHITE)
+                    }
+
+                    binding.btnGenerateCode.setOnClickListener{
+                        setFrag(1)
+                        binding.btnEnterCode.setTextColor(Color.WHITE)
+                        binding.btnGenerateCode.setTextColor(Color.parseColor("#FFE600"))
+                    }
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+
+    private fun getUserUIDFromMeetups(invitationCode : String){
+        val meetupsRef = database.child("meetups").child(invitationCode)
+
+        meetupsRef.child("users").addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val currentUserUID = auth.currentUser?.uid
+
+                if (currentUserUID != null && dataSnapshot.hasChild(currentUserUID)){
+
+                    val intent = Intent(this@InviteCodeActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                } else {
+                    setFrag(0)
+                    binding.btnEnterCode.setTextColor(Color.parseColor("#FFE600"))
+
+
+                    binding.btnEnterCode.setOnClickListener{
+                        setFrag(0)
+                        binding.btnEnterCode.setTextColor(Color.parseColor("#FFE600"))
+                        binding.btnGenerateCode.setTextColor(Color.WHITE)
+                    }
+
+                    binding.btnGenerateCode.setOnClickListener{
+                        setFrag(1)
+                        binding.btnEnterCode.setTextColor(Color.WHITE)
+                        binding.btnGenerateCode.setTextColor(Color.parseColor("#FFE600"))
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+
+
 
     private fun setFrag(fragNum : Int) {
         val ft = supportFragmentManager.beginTransaction()
